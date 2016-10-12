274ae0455d70155f9d41f1b1a2400ba1ba950272
developer <dev@developer.loc>
2016-10-12 12:19:01 +0300

    Add gitignore

--- polls/models.py
+++ polls/models.py
@@ -8,3 +8,4 @@ class Choice(models.Model):
     question=models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    upd_date = models.DateTimeField('date update')
