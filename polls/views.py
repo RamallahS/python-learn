5b37d2794caa233aa1c43a4343daebf0938b253b
developer <dev@developer.loc>
2016-10-12 13:56:31 +0300

    ...

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,20 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+
+from .models import Question
 
 
+# render variant 1
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    questions_list = Question.objects.all()
+    template = loader.get_template('index.html')
+    context = {"questions_list": questions_list}
+    return HttpResponse(template.render(context, request))
+
+
+# render variant 2
+def index2(request):
+    questions_list = Question.objects.all()
+    context = {"questions_list": questions_list}
+    return render(request, 'index.html', context)
