11ca91465626f3abe17e04b8963638664d59de7c
Alex German <alex.germannn@gmail.com>
2016-10-13 20:00:04 +0300

    Add Livereload

--- mysite/settings.py
+++ mysite/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'livereload',
 ]
 
 MIDDLEWARE = [
@@ -62,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
--- read.md
+++ read.md
@@ -5,5 +5,9 @@
 https://github.com/owais/django-webpack-loader
 `sudo npm install webpack -g`
 `sudo pip install django-webpack-loader`
+## Livereload
+`pip install django-livereload-server`
+Run Liverload server:
+`python3.4 ./manage.py livereload`
 ## NPM packages
 `npm install`
