c59c8e8c8a5db7d6f991ecbc1635e9ebeff5ec91
Alex German <alex.germannn@gmail.com>
2016-10-17 17:32:57 +0300

    Work with forms

--- polls/modules/dashboard/animal_views.py
+++ polls/modules/dashboard/animal_views.py
@@ -1,3 +1,4 @@
+from django.template.loader_tags import register
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.views.generic import DeleteView
@@ -6,6 +7,26 @@ from django.views.generic import UpdateView
 
 from polls.models import Animal
 
+# Forms classes
+from django import forms
+
+
+class AnimalForm(forms.ModelForm):
+    name = forms.CharField(min_length=2)
+    animal_kind = forms.Select()
+
+    class Meta:
+        model = Animal
+        fields = ['name', 'animal_kind']
+
+
+# Filters
+# Пример регистрации и использования фильтров
+@register.filter(is_safe=True)
+def label_with_classes(value, arg):
+    return value.label_tag(attrs={'class': arg})
+
+# Views classes
 
 class AnimalMixin(object):
     model = Animal
@@ -17,11 +38,13 @@ class AnimalList(AnimalMixin, ListView):
 
 
 class AnimalCreate(AnimalMixin, CreateView):
-    fields = ['name', 'animal_kind']
+    form_class = AnimalForm
+    # fields = ['name', 'animal_kind']
 
 
 class AnimalUpdate(AnimalMixin, UpdateView):
-    fields = ['name', 'animal_kind']
+    form_class = AnimalForm
+    # fields = ['name', 'animal_kind']
 
 
 class AnimalDelete(AnimalMixin, DeleteView):
